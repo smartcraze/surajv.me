fce3d225cf157fe80de427c03e8ae68d6db46ddf
Suraj Vishwakarma <smartcraze354@gmail.com>
2024-04-23 00:00:17 +0530

    Update page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -26,9 +26,9 @@ function MusicSchoolContactUs() {
           Contact Us
         </h1>
         <p className="text-neutral-500 max-w-lg mx-auto my-2 text-sm text-center">
-          We&apos;re here to help with any questions about our courses,
-          programs, or events. Reach out and let us know how we can assist you
-          in your musical journey.
+          We&apos;re here to help with any questions about our Academic,
+         Reach out and let us know how we can assist you
+          in your Programming journey.
         </p>
         <form onSubmit={handleSubmit} className="space-y-4 mt-4">
           <input
