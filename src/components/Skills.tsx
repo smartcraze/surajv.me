456f0387faab1e0f9614e36248aeadb953d64a8e
suraj vishwakarma <smartcraze354@gmail.com>
2025-05-01 17:19:50 +0000

    responsivness

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -20,45 +20,46 @@ function Skill() {
   );
 
   const iconMap: { [key: string]: JSX.Element } = {
-    FaPython: <FaPython size={50} className="text-[#3776AB]" />,
-    FaJava: <FaJava size={50} className="text-[#007396]" />,
-    FaGithub: <FaGithub size={50} className="text-white" />,
-    FaGitAlt: <FaGitAlt size={50} className="text-[#F05032]" />,
-    FaReact: <FaReact size={50} className="text-[#61DAFB]" />,
-    FaNodeJs: <FaNodeJs size={50} className="text-[#339933]" />,
-    FaDocker: <FaDocker size={50} className="text-[#2496ED]" />,
-    FaAws: <FaAws size={50} className="text-[#FF9900]" />,
-    SiCplusplus: <SiCplusplus size={50} className="text-[#00599C]" />,
-    SiC: <SiC size={50} className="text-[#A8B9CC]" />,
-    SiJavascript: <SiJavascript size={50} className="text-[#F7DF1E]" />,
-    SiMongodb: <SiMongodb size={50} className="text-[#47A248]" />,
-    SiNextdotjs: <SiNextdotjs size={50} className="text-white" />,
-    SiHono: <SiHono size={50} className="text-[#ff6b81]" />,
-    SiPostgresql: <SiPostgresql size={50} className="text-[#336791]" />,
+    FaPython: <FaPython size={40} className="text-[#3776AB]" />,
+    FaJava: <FaJava size={40} className="text-[#007396]" />,
+    FaGithub: <FaGithub size={40} className="text-white" />,
+    FaGitAlt: <FaGitAlt size={40} className="text-[#F05032]" />,
+    FaReact: <FaReact size={40} className="text-[#61DAFB]" />,
+    FaNodeJs: <FaNodeJs size={40} className="text-[#339933]" />,
+    FaDocker: <FaDocker size={40} className="text-[#2496ED]" />,
+    FaAws: <FaAws size={40} className="text-[#FF9900]" />,
+    SiCplusplus: <SiCplusplus size={40} className="text-[#00599C]" />,
+    SiC: <SiC size={40} className="text-[#A8B9CC]" />,
+    SiJavascript: <SiJavascript size={40} className="text-[#F7DF1E]" />,
+    SiMongodb: <SiMongodb size={40} className="text-[#47A248]" />,
+    SiNextdotjs: <SiNextdotjs size={40} className="text-white" />,
+    SiHono: <SiHono size={40} className="text-[#ff6b81]" />,
+    SiPostgresql: <SiPostgresql size={40} className="text-[#336791]" />,
   };
 
   return (
     <div className="py-12 bg-gray-900">
-      <div>
-        <div className="text-center">
-          <p id="skill" className="mt-2 text-3xl text-teal-600 leading-8 font-extrabold tracking-tight sm:text-4xl">
-            Skills I know
-          </p>
-        </div>
+      <div className="text-center">
+        <p id="skill" className="mt-2 text-3xl text-teal-600 leading-8 font-extrabold tracking-tight sm:text-4xl">
+          Skills I know
+        </p>
       </div>
-      <div className="mt-10 mx-8">
-        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-6">
+
+      <div className="mt-10 px-4 sm:px-6 lg:px-8">
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6 justify-items-center">
           {featuredCourses.map((skill: skills) => (
-            <div key={skill.id} className="hover:transform hover:scale-105 transition-transform duration-300">
-              <BackgroundGradient className="rounded-[22px] max-w-sm p-4 sm:p-10 bg-slate-950 dark:bg-zinc-900 hover:will-change-transform flex flex-col items-center justify-center text-center w-40 h-40">
-                <div className="mb-4 flex items-center justify-center">
-                  {iconMap[skill.icon]}
-                </div>
-                <h2 className="text-base text-indigo-200 font-semibold tracking-wide uppercase">
-                  {skill.title}
-                </h2>
-              </BackgroundGradient>
-            </div>
+            <BackgroundGradient
+              key={skill.id}
+              className="rounded-[22px] w-[140px] h-[160px] p-4 bg-slate-950 dark:bg-zinc-900  flex flex-col items-center justify-center text-center hover"
+            >
+              <div className="flex items-center justify-center h-[60px] hover:scale-110 transition-transform">
+                {iconMap[skill.icon]}
+              </div>
+              <h2 className="text-sm text-indigo-200 font-semibold tracking-wide uppercase">
+                {skill.title}
+              </h2>
+            </BackgroundGradient>
+
           ))}
         </div>
       </div>
