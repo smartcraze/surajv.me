99b7381c30f9bec9925eea7bfee2522e716f62c5
suraj vishwakarma <smartcraze354@gmail.com>
2025-05-01 08:42:51 +0000

    title changed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
-  title: "Smartcraze - Suraj Vishwakarma",
+  title: "Suraj Vishwakarma|smartcraze",
   description: "Portfolio of Suraj Vishwakarma Cse",
   keywords: ["portfolio", "computer science", "engineering", "Suraj Vishwakarma"],
   // authors: "Suraj Vishwakarma",
