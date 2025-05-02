406a96fe9487c1c07941275f1b29e28222a1fccf
Suraj Vishwakarma <surajvishwakarma23@lpu.in>
2025-05-02 11:19:14 +0530

    Update Skills.tsx

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -51,7 +51,7 @@ function Skill() {
       </div>
 
       <div className="mt-10 px-4 sm:px-6 lg:px-8">
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6 justify-items-center">
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-3 justify-items-center">
           {featuredCourses.map((skill: skills) => (
             <BackgroundGradient
               key={skill.id}
