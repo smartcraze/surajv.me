bc63087ea639b4e3d497d470f12b2bc4933df4be
suraj vishwakarma <smartcraze354@gmail.com>
2025-06-12 15:32:52 +0530

    add video in the projects

--- lib/projects.ts
+++ lib/projects.ts
@@ -5,57 +5,55 @@ interface Project {
   status: string;
   github: string;
   live: string;
-}   
+}
 
 export const projects: Project[] = [
   {
-    slug: "token-launchpad",
-    title: "Token Launchpad",
-    video: "https://www.tella.tv/video/cmbsyno0t000t0bky1lg412i3/embed?b=0&title=0&a=0&loop=0&t=0&muted=0&wt=0",
-    status: "Completed",
-    github: "https://github.com/smartcraze/100xdev-web3/tree/main/token-lounchpad-2",
-    live: "",
-  },
-  {
     slug: "paytm",
     title: "Paytm Clone",
-   
+
     video: "https://www.tella.tv/video/cmbqd5mpb00000bl52thb4njf/embed?b=0&title=0&a=0&loop=0&t=0&muted=0&wt=0",
-    status: "Completed",
-   
+    status: "web-dev",
+
     github: "https://github.com/smartcraze/paytm-clone",
     live: "https://paytm.surajv.me",
   },
   {
     slug: "cursorbot",
     title: "CursorBot",
-   
-    video: "https://www.tella.tv/video/cmbmiebi800100cju3uv7alr1/embed?b=0&title=0&a=1&loop=0&t=0&muted=0&wt=0", 
-    status: "Learning",
-   
+
+    video: "https://www.tella.tv/video/cmbmiebi800100cju3uv7alr1/embed?b=0&title=0&a=1&loop=0&t=0&muted=0&wt=0",
+    status: "Ai wrapper",
+
     github: "https://github.com/smartcraze/cursorbot",
     live: "https://cursorbot.surajv.me",
   },
   {
     slug: "ezsnipe",
     title: "EzSnipe CLI",
-   
-    video: "https://www.tella.tv/video/cmbmiebi800100cju3uv7alr1/embed?b=0&title=0&a=1&loop=0&t=0&muted=0&wt=0", 
-    status: "Completed",
-   
+    video: "https://www.tella.tv/video/cmbt6qj4n000i0blbfbd7dmdv/embed?b=0&title=0&a=0&loop=0&t=0&muted=0&wt=0",
+    status: "npm-package",
     github: "https://github.com/smartcraze/ezsnipe-npm-package",
     live: "https://www.npmjs.com/package/ezsnipe",
   },
- 
+
   {
     slug: "rapidserve",
     title: "RapidServe",
-   
+
     video: "https://www.tella.tv/video/cmbmiebi800100cju3uv7alr1/embed?b=0&title=0&a=1&loop=0&t=0&muted=0&wt=0",
-    status: "Completed",
-   
+    status: "web-dev",
+
     github: "https://github.com/smartcraze/vercel-deploy",
     live: "https://rapidserve.surajv.me",
-  }
+  },
+  {
+    slug: "token-launchpad",
+    title: "Token Launchpad",
+    video: "https://www.tella.tv/video/cmbsyno0t000t0bky1lg412i3/embed?b=0&title=0&a=0&loop=0&t=0&muted=0&wt=0",
+    status: "web3",
+    github: "https://github.com/smartcraze/100xdev-web3/tree/main/token-lounchpad-2",
+    live: "",
+  },
 ];
 
