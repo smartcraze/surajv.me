5ba4b81a30bfecb0722dbebdd81fdb834483ffa5
suraj vishwakarma <smartcraze354@gmail.com>
2025-06-20 19:29:27 +0530

    what i am learning

--- lib/what-im-learning.ts
+++ lib/what-im-learning.ts
@@ -9,6 +9,10 @@ export const learning: Learning[] = [
         link: "https://github.com/smartcraze/100xdev-web3"
     },
     {
+        title: "Langchain Generative AI",
+        link: "https://github.com/smartcraze/langchain"
+    },
+    {
         title: "Fullstack projects driven learning Both frontend and backend(node)",
         link: "https://github.com/smartcraze/100xdev-webcohort"
     },
