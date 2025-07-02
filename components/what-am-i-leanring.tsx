7664c33e9d41ea7e82361f9e9e18ff2a8cf177f7
suraj vishwakarma <smartcraze354@gmail.com>
2025-07-02 12:44:26 +0530

    chnaged the icon

--- components/what-am-i-leanring.tsx
+++ components/what-am-i-leanring.tsx
@@ -2,19 +2,21 @@ import { learning } from "@/lib/what-im-learning";
 import { ArrowRight, Github } from "lucide-react";
 import Link from "next/link";
 import { Metadata } from 'next';
+import {Learning as LearningType} from "@/lib/what-im-learning";
 
 export const metadata: Metadata = {
   title: "What I'm Learning",
   description: "Topics and resources I'm currently exploring",
 };
 
-export function LearningCard({ title, link }: { title: string; link: string }) {
+export function LearningCard({ title, link ,icon:Icon}: LearningType) {
   return (
     <Link
       href={link}
       className="w-full border border-zinc-600 rounded-md p-2 flex flex-row gap-2 items-center text-zinc-900 dark:text-zinc-100 hover:bg-gradient-to-r hover:from-zinc-100 hover:to-indigo-50 dark:hover:from-zinc-800 dark:hover:to-indigo-900/30 transition-all duration-200 ease-in-out group hover:shadow-md hover:scale-[1.02] hover:-translate-y-[2px]"
     >
-      <Github className="w-4 h-4 text-zinc-400 group-hover:text-indigo-500 dark:group-hover:text-indigo-400 transition-colors duration-200" />
+      <Icon className="w-4 h-4 text-zinc-400 group-hover:text-indigo-500 dark:group-hover:text-indigo-400 transition-colors duration-200" />
+
       <span className="font-semibold transition-colors duration-200">
         {title}
       </span>
@@ -29,7 +31,7 @@ export default function WhatAmILearning() {
       <h2 className="text-2xl font-bold pb-2 text-zinc-900 dark:text-zinc-100">What I&apos;m Learning</h2>
       <div className="flex flex-col gap-3 max-w-3xl mx-auto">
         {learning.map((x, index) => (
-          <LearningCard key={index} title={x.title} link={x.link} />
+          <LearningCard key={index} title={x.title} link={x.link} icon={x.icon}/>
         ))}
       </div>
     </div>
--- lib/what-im-learning.ts
+++ lib/what-im-learning.ts
@@ -1,23 +1,36 @@
-interface Learning {
+import { Dock, DockIcon, Github, ServerCrash } from "lucide-react";
+
+export interface Learning {
     title: string;
     link: string;
+    icon: React.ElementType;
 }
 
 export const learning: Learning[] = [
     {
         title: "Blockchain Development(solana)",
-        link: "https://github.com/smartcraze/100xdev-web3"
+        link: "https://github.com/smartcraze/100xdev-web3",
+        icon: Github
+
     },
     {
         title: "Langchain Generative AI",
-        link: "https://github.com/smartcraze/Genrative-AI"
+        link: "https://github.com/smartcraze/Genrative-AI",
+        icon: Github
     },
     {
         title: "Fullstack projects driven learning Both frontend and backend(node)",
-        link: "https://github.com/smartcraze/100xdev-webcohort"
+        link: "https://github.com/smartcraze/100xdev-webcohort",
+        icon: Github
     },
     {
         title: "Docker Containering multiple applications",
-        link: "https://github.com/smartcraze/devops-assignment"
+        link: "https://github.com/smartcraze/devops-assignment",
+        icon: DockIcon
+    },
+    {
+        title: "GitHub Actions Cron Job Exhausted My Render Free Tier",
+        link: "https://github.com/whateverui/cron-job/actions",
+        icon: ServerCrash
     }
-]
+]   
