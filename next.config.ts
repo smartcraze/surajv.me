7dff93deda8454ac5afcbf3648686a77ea7bfbe4
suraj vishwakarma <smartcraze354@gmail.com>
2025-08-13 16:13:44 +0530

    added shortcut link for the social media

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,35 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/instagram",
+        destination: "https://instagram.com/surajv354",
+        permanent: false,
+      },
+      {
+        source: "/x",
+        destination: "https://x.com/surajv354",
+        permanent: true,
+      },
+      {
+        source: "/twitter", 
+        destination: "https://x.com/surajv354",
+        permanent: true,
+      },
+      {
+        source: "/github",
+        destination: "https://github.com/smartcraze",
+        permanent: true,
+      },
+      {
+        source: "/linkedin",
+        destination: "https://linkedin.com/in/surajv354",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
