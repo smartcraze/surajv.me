9050713e11c92fbe3cd9c9cea975ae09f500b5ed
Suraj Vishwakarma <surajvishwakarma23@lpu.in>
2025-08-24 16:16:49 +0530

    Add calendar embed component to meet page

---
+++ app/meet/page.tsx
@@ -0,0 +1,19 @@
+"use client"
+import Cal, { getCalApi } from "@calcom/embed-react";
+import { useEffect } from "react";
+export default function MyApp() {
+  useEffect(() => {
+    (async function () {
+      const cal = await getCalApi({"namespace":"30min"});
+      cal("ui", {"hideEventTypeDetails":false,"layout":"month_view"});
+    })();
+  }, [])
+  return <Cal namespace="30min"
+    calLink="surajv354/30min"
+    style={{width:"100%",height:"100%",overflow:"scroll"}}
+    config={{"layout":"month_view"}}
+    
+    
+  />;
+};
+  
