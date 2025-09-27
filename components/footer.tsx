cfe0ad108fb9d7c5fdc8594c681a385500f7e096
suraj vishwakarma <smartcraze354@gmail.com>
2025-09-27 18:03:13 +0530

    Implement code changes to enhance functionality and improve performance

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,54 +1,56 @@
-import React from 'react'
-import Link from 'next/link'
-import {
-    Github,
-    Instagram,
-    Linkedin,
-    Twitter,
-    Mail,
-} from 'lucide-react'
+import React from "react";
+import Link from "next/link";
+import { Github, Instagram, Linkedin, Twitter, Mail } from "lucide-react";
+import Image from "next/image";
 
 function Footer() {
   return (
-    <footer className="flex flex-col gap-4 text-zinc-400">    
-        <div className="flex justify-center items-center gap-4">
-            <Link 
-              href="https://github.com/smartcraze"
-              className="hover:text-white transition-all duration-200 hover:scale-110 hover:-translate-y-0.5"
-            >
-              <Github />
-            </Link>
-            <Link 
-              href="mailto:dev.surajv@gmail.com"
-              className="hover:text-white transition-all duration-200 hover:scale-110 hover:-translate-y-0.5"
-            >
-              <Mail />
-            </Link>
-            <Link 
-              href="https://www.instagram.com/surajv354"
-              className="hover:text-pink-500 transition-all duration-200 hover:scale-110 hover:-translate-y-0.5"
-            >
-              <Instagram />
-            </Link>
-            <Link 
-              href="https://www.linkedin.com/in/surajv354"
-              className="hover:text-blue-500 transition-all duration-200 hover:scale-110 hover:-translate-y-0.5"
-            >
-              <Linkedin />
-            </Link>
-            <Link 
-              href="https://x.com/surajv354"
-              className="hover:text-blue-400 transition-all duration-200 hover:scale-110 hover:-translate-y-0.5"
-            >
-              <Twitter />
-            </Link>
-        </div>
-        <div className="text-center">
-          &copy; {new Date().getFullYear()} suraj vishwakarma. All rights reserved.
-        </div>
-        <div></div>
+    <footer className="flex flex-col gap-4 text-zinc-400">
+      <div className="flex justify-center items-center gap-4">
+        <Link
+          href="https://github.com/smartcraze"
+          className="hover:text-white transition-all duration-200 hover:scale-110 hover:-translate-y-0.5"
+        >
+          <Github />
+        </Link>
+        <Link
+          href="mailto:dev.surajv@gmail.com"
+          className="hover:text-white transition-all duration-200 hover:scale-110 hover:-translate-y-0.5"
+        >
+          <Mail />
+        </Link>
+        <Link
+          href="https://www.instagram.com/surajv354"
+          className="hover:text-pink-500 transition-all duration-200 hover:scale-110 hover:-translate-y-0.5"
+        >
+          <Instagram />
+        </Link>
+        <Link
+          href="https://www.linkedin.com/in/surajv354"
+          className="hover:text-blue-500 transition-all duration-200 hover:scale-110 hover:-translate-y-0.5"
+        >
+          <Linkedin />
+        </Link>
+        <Link
+          href="https://x.com/surajv354"
+          className="hover:text-blue-400 transition-all duration-200 hover:scale-110 hover:-translate-y-0.5"
+        >
+          <Twitter />
+        </Link>
+      </div>
+      <div className="text-center">
+        &copy; {new Date().getFullYear()} suraj vishwakarma. All rights
+        reserved.
+      </div>
+      <Image
+        src={"/linkedin-headers.png"}
+        alt="logo"
+        width={200}
+        height={200}
+        className="w-full h-auto mx-auto rounded-lg"
+      />
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
